904507ad1ce9d72f5ce72973811e073e2206c9f4
Henrique Rodrigues <henrique.93.rodrigues@gmail.com>
2017-11-19 22:23:06 -0200

    change import mode

--- Makefile
+++ Makefile
@@ -14,3 +14,6 @@ elixir/lib/elixir/ebin/elixir.app:
 	git clone https://github.com/elixir-lang/elixir.git
 	echo "start to build elixir ..."
 	make -C elixir -f Makefile
+
+clean:
+	rm -rf _build deps
--- lib/emq_throttle_plugin/plugin_shared.ex
+++ lib/emq_throttle_plugin/plugin_shared.ex
@@ -1,6 +1,6 @@
 defmodule EmqThrottlePlugin.Shared do
   require Record
   import Record, only: [defrecord: 2, extract: 2]
-  defrecord :mqtt_client, extract(:mqtt_client, from_lib: "emqttd/include/emqttd.hrl")
+  defrecord :mqtt_client, extract(:mqtt_client, from: "deps/emqttd/include/emqttd.hrl")
   defrecord :state, [:auth_cmd, :super_cmd, :hash_type]
 end
