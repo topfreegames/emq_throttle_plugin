1421009fcba4b38f818d96a80d4453d77bb533d8
Henrique Rodrigues <henrique.93.rodrigues@gmail.com>
2017-11-20 11:24:01 -0200

    etc dir and correct config

--- config/config.exs
+++ config/config.exs
@@ -28,14 +28,3 @@ use Mix.Config
 # here (which is why it is important to import them last).
 #
 #     import_config "#{Mix.env}.exs"
-
-config :redis_connection_pool, [
-  host: "127.0.0.1",
-  port: 6379,
-  password: "",
-  db: 0,
-  reconnect: :no_reconnect,
-  pool_name: :"Redis.Pool",
-  pool_size: 10,
-  pool_max_overflow: 1
-]
