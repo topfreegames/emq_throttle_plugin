f701962e54b0faa6b6ac75f7b5e7051245d4a4ba
Henrique Rodrigues <henrique.93.rodrigues@gmail.com>
2017-11-21 19:22:09 -0200

    use of logger

--- lib/emq_throttle_plugin/plugin_redis.ex
+++ lib/emq_throttle_plugin/plugin_redis.ex
@@ -1,11 +1,15 @@
 defmodule EmqThrottlePlugin.Redis do
+  require Logger
+
   @redis_instances String.to_integer(System.get_env("REDIS_AUTH_REDIS_POOL_SIZE") || "5")
 
   def command(command) do
     case Redix.command(:"redix_#{random_index()}", command) do
       {:ok, result} -> result
       {_, message} -> 
-        IO.puts message
+        Logger.error fn ->
+          "redis error: #{message}"
+        end
         nil
     end
   end
@@ -14,7 +18,9 @@ defmodule EmqThrottlePlugin.Redis do
     case Redix.pipeline(:"redix_#{random_index()}", pipeline) do
       {:ok, result} -> result
       {_, message} -> 
-        IO.puts message
+        Logger.error fn ->
+          "redis error: #{message}"
+        end
         nil
     end
   end
